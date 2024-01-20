77a93c1d6f09ef4012ab245082fd1057b15d1dc3
afzal6870 <afzal.aslam0786@gmail.com>
2024-01-20 23:22:21 +0500

    first commit

---
+++ assets/css/style.cs
@@ -0,0 +1,17 @@
+/* Error: Expected digit.
+ *     ,
+ * 481 |         .navigations-data {
+ *     |          ^
+ *     '
+ *   style.scss 481:10  root stylesheet */
+
+body::before {
+  font-family: "Source Code Pro", "SF Mono", Monaco, Inconsolata, "Fira Mono",
+      "Droid Sans Mono", monospace, monospace;
+  white-space: pre;
+  display: block;
+  padding: 1em;
+  margin-bottom: 1em;
+  border-bottom: 2px solid black;
+  content: "Error: Expected digit.\a     \2577 \a 481 \2502          .navigations-data {\d\a     \2502           ^\a     \2575 \a   style.scss 481:10  root stylesheet";
+}
